c89f3f08163b37c0d399d42b59fbb42217219db0
Olivier <oli1999@hotmail.ca>
2023-08-14 09:10:07 -0400

    feature-gridMeal-structureBase

--- src/components/AffichageData/AffichageGridMeal.vue
+++ src/components/AffichageData/AffichageGridMeal.vue
@@ -8,21 +8,64 @@
 
         <div class="grid">
             <div class="groupe1">
+                <h2>1er &amp; 5e vendredi</h2>
                 <div class="meal1">
-                
+                    <h3>meal1</h3>
+                    <div class="warpImg">
+                        <img src="@/assets/img/no-img.png" alt="img-placeholder">
+                    </div>
                 </div>
                 <div class="meal2">
-                
+                    <h3>meal2</h3>
+                    <div class="warpImg">
+                        <img src="@/assets/img/no-img.png" alt="img-placeholder">
+                    </div>
                 </div>
             </div>
             <div class="groupe2">
-            
+                <h2>2e vendredi</h2>
+                <div class="meal1">
+                    <h3>meal1</h3>
+                    <div class="warpImg">
+                        <img src="@/assets/img/no-img.png" alt="img-placeholder">
+                    </div>
+                </div>
+                <div class="meal2">
+                    <h3>meal2</h3>
+                    <div class="warpImg">
+                        <img src="@/assets/img/no-img.png" alt="img-placeholder">
+                    </div>
+                </div>
             </div>
             <div class="groupe3">
-            
+                <h2>3e vendredi</h2>
+                <div class="meal1">
+                    <h3>meal1</h3>
+                    <div class="warpImg">
+                        <img src="@/assets/img/no-img.png" alt="img-placeholder">
+                    </div>
+                </div>
+                <div class="meal2">
+                    <h3>meal2</h3>
+                    <div class="warpImg">
+                        <img src="@/assets/img/no-img.png" alt="img-placeholder">
+                    </div>
+                </div>
             </div>
             <div class="groupe4">
-            
+                <h2>4e vendredi</h2>
+                <div class="meal1">
+                    <h3>meal1</h3>
+                    <div class="warpImg">
+                        <img src="@/assets/img/no-img.png" alt="img-placeholder">
+                    </div>
+                </div>
+                <div class="meal2">
+                    <h3>meal2</h3>
+                    <div class="warpImg">
+                        <img src="@/assets/img/no-img.png" alt="img-placeholder">
+                    </div>
+                </div>
             </div>
         </div>
 
