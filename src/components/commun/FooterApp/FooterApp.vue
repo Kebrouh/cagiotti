0599d13202328e2ef1732fb63ff97e89784a221d
Olivier <oli1999@hotmail.ca>
2023-08-14 10:08:10 -0400

    feature-footer-posBase

--- src/components/commun/FooterApp/FooterApp.vue
+++ src/components/commun/FooterApp/FooterApp.vue
@@ -5,7 +5,16 @@
 <template>
     <div class="footer">
 
-        <h3>Footer</h3>
+        <div class="wrapInfo">
+            <h3>Contacts :</h3>
+            <p>email</p>
+            <p>num cell 1</p>
+            <p>num cell 2</p>
+        </div>
+
+        <div class="wrapLogo">
+            <img src="@/assets/img/no-img.png" alt="logo">
+        </div>
 
     </div>
 </template>
