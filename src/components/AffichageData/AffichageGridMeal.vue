cac110301e384f2ccf3c93fca661cef8b134df9f
Kebrouh at school <oli1999@hotmail.ca>
2023-08-28 14:05:27 -0400

    feature-gridMeal-try1DayMethod

--- src/components/AffichageData/AffichageGridMeal.vue
+++ src/components/AffichageData/AffichageGridMeal.vue
@@ -9,15 +9,24 @@
     const currentDayOfWeek = ref(daysOfWeek[currentDate.getDay()]);
     const currentNumOfWeek = ref(currentDate.getDay());
 
-    // Function to determine whether a group should move to the back
-    const shouldMoveToBack = (groupNumber) => {
-    if (currentDayOfWeek === 'Monday') {
-        // Calculate new group order based on currentNumOfWeek
-        const newGroupOrder = (groupNumber + currentNumOfWeek - 1) % 4;
-        return newGroupOrder === 0;
-    }
-    return false;
+    console.log("hello  yes i am awake");
+
+    const shouldMoveToBack = () => {
+        console.log("skadoosh");
+        return currentDayOfWeek.value === 'Monday';
     };
+
+    // Function to determine whether a group should move to the back
+    // const shouldMoveToBack = (groupNumber) => {
+    //     if (currentDayOfWeek.value === 'Monday') { // Fix this line
+    //         const currentWeekDayNumber = currentNumOfWeek.value;
+    //         const newGroupOrder = (groupNumber + currentWeekDayNumber - 1) % 4;
+    //         console.log(groupNumber + " + " + currentNumOfWeek.value + " = " + newGroupOrder);
+    //         return newGroupOrder === 0;
+    //     }
+    //     console.log("cringe lmao");
+    //     return false;
+    // };
 </script>
 
 <template>
@@ -37,13 +46,13 @@
             <div class="groupe1" :class="{'moveToBack': shouldMoveToBack(1)}">
                 <h2>groupe1</h2>
                 <div class="meal1">
-                    <h3>meal1</h3>
+                    <h3>G1-M1</h3>
                     <div class="wrapImg">
                         <img src="@/assets/img/no-img.png" alt="img-placeholder">
                     </div>
                 </div>
                 <div class="meal2">
-                    <h3>meal2</h3>
+                    <h3>G1-M2</h3>
                     <div class="wrapImg">
                         <img src="@/assets/img/no-img.png" alt="img-placeholder">
                     </div>
@@ -52,13 +61,13 @@
             <div class="groupe2" :class="{'moveToBack': shouldMoveToBack(2)}">
                 <h2>groupe2</h2>
                 <div class="meal1">
-                    <h3>meal1</h3>
+                    <h3>G2-M1</h3>
                     <div class="wrapImg">
                         <img src="@/assets/img/no-img.png" alt="img-placeholder">
                     </div>
                 </div>
                 <div class="meal2">
-                    <h3>meal2</h3>
+                    <h3>G2-M2</h3>
                     <div class="wrapImg">
                         <img src="@/assets/img/no-img.png" alt="img-placeholder">
                     </div>
@@ -67,13 +76,13 @@
             <div class="groupe3" :class="{'moveToBack': shouldMoveToBack(3)}">
                 <h2>groupe3</h2>
                 <div class="meal1">
-                    <h3>meal1</h3>
+                    <h3>G3-M1</h3>
                     <div class="wrapImg">
                         <img src="@/assets/img/no-img.png" alt="img-placeholder">
                     </div>
                 </div>
                 <div class="meal2">
-                    <h3>meal2</h3>
+                    <h3>G3-M2</h3>
                     <div class="wrapImg">
                         <img src="@/assets/img/no-img.png" alt="img-placeholder">
                     </div>
@@ -82,13 +91,13 @@
             <div class="groupe4" :class="{'moveToBack': shouldMoveToBack(4)}">
                 <h2>groupe4</h2>
                 <div class="meal1">
-                    <h3>meal1</h3>
+                    <h3>G4-M1</h3>
                     <div class="wrapImg">
                         <img src="@/assets/img/no-img.png" alt="img-placeholder">
                     </div>
                 </div>
                 <div class="meal2">
-                    <h3>meal2</h3>
+                    <h3>G4-M2</h3>
                     <div class="wrapImg">
                         <img src="@/assets/img/no-img.png" alt="img-placeholder">
                     </div>
@@ -101,5 +110,16 @@
 </template>
 
 <style scoped>
+    .grid {
+        display: flex;
+    }
 
+    .groupe1, .groupe2, .groupe3, .groupe4 {
+        flex: 1;
+        transition: order 1s ease-in-out; /* Adjust the duration and timing function as needed */
+    }
+
+    .moveToBack {
+        order: -1;
+    }
 </style>
