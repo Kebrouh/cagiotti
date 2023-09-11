96f73fda5462b97ea83e059b26f7900641416105
Olivier <oli1999@hotmail.ca>
2023-09-11 14:50:06 -0400

    fix-map-correctionAdresse

--- src/components/AffichageData/AffichageWrapMap.vue
+++ src/components/AffichageData/AffichageWrapMap.vue
@@ -7,7 +7,7 @@
     <div class="wrap-map">
         <div class="wrapInfo">
             <h3>Pour le ramassage de la commande</h3>
-            <h2>569 Rue de la Charente</h2>
+            <h2>168 Rue de la Charente</h2>
         </div>
         <div class="wrapImg">
             <img src="@/assets/img/map_v2.svg" alt="img-placeholder">
